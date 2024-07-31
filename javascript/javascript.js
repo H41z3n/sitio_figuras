2171a18f42fe98dd19e7b079d269acb28cd78173
H41z3n <aj.elctromec@gmail.com>
2024-07-31 14:26:58 +0200

    modo daltonismo implementado

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,1 +1,7 @@
 // JavaScript Document
+
+// Activar daltonismo
+document.getElementById('toggleDaltonismMode').addEventListener('click', function() {
+    document.body.classList.toggle('daltonism-mode');
+  });
+  
